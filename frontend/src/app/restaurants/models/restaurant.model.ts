44c9527b28645679761c426f2ebad7405adf3074
Vanesa <vvalverde76@gmail.com>
2023-05-25 19:11:28 +0200

    atributos de users y restaurants

--- frontend/src/app/restaurants/models/restaurant.model.ts
+++ frontend/src/app/restaurants/models/restaurant.model.ts
@@ -1,2 +1,13 @@
 export interface IRestaurant {
+    id: number;
+    name: string;
+    direction: string;
+    phone: string;
+    web: string;
+    mail: string;
+    averagePrice: number;
+    rating: number;
+    availability: boolean;
+    typeFood: string;
+
 }
--- frontend/src/app/users/models/user.model.ts
+++ frontend/src/app/users/models/user.model.ts
@@ -1,2 +1,9 @@
 export interface IUser {
+    id: number;
+    firstName: string;
+    lastName: string;
+    alias: string;
+    phone: string;
+    email: string;
+    avatar: string;
 }
