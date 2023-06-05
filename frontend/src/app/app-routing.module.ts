6d42b53bc6b4b7be3946bad0a096a4c4744b2e09
gloriafercu <gloriafercu@gmail.com>
2023-06-05 18:09:36 +0200

    actualización del routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,16 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-  path: 'restaurants', // pagina home / pagina de inicio
-  loadChildren: () => import('./restaurants/restaurants.module').then(m => m.RestaurantsModule)
-},
+    path: 'restaurants', // pagina home / pagina de inicio
+    loadChildren: () => import('./restaurants/restaurants.module').then(m => m.RestaurantsModule)
+  },
 
-{
-  path: '', redirectTo:'restaurants', pathMatch: 'full' // redireccion a pag principal
-},
-{
-  path: '**', redirectTo:'restaurants', pathMatch: 'full' //redireccion para paginas que no existen
-}
+  {
+    path: '', redirectTo: 'restaurants', pathMatch: 'full' // redireccion a pag principal
+  },
+  {
+    path: '**', redirectTo: 'restaurants', pathMatch: 'full' //redireccion para paginas que no existen
+  }
 ];
 
 @NgModule({
