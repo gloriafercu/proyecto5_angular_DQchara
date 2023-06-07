88121dc50177c969b15c49c61e6b01e50a6bb46e
Vanesa <vvalverde76@gmail.com>
2023-06-07 19:32:34 +0200

    generate de componentes y servicio de usuarios

--- frontend/src/app/users/users.module.ts
+++ frontend/src/app/users/users.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { UserFormComponent } from './user-form/user-form.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserDetailComponent,
+    UserFormComponent,
+    UserListComponent
+  ],
   imports: [
     CommonModule,
     UsersRoutingModule
