2faebc6e1051e3375459135174d0f8c6e6784800
vcrl1 <vcrl1994@gmail.com>
2023-06-13 13:07:35 +0200

    comentarios usuarios

--- frontend/src/app/restaurants/restaurants.module.ts
+++ frontend/src/app/restaurants/restaurants.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RestaurantsRoutingModule } from './restaurants-routing.module';
 import { MatCardModule } from '@angular/material/card';
-import { MatGridListModule} from '@angular/material/grid-list';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatPaginatorModule } from '@angular/material/paginator'
+import { MatListModule } from '@angular/material/list';
 
 import { RestaurantListComponent } from './restaurant-list/restaurant-list.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
@@ -15,7 +16,7 @@ import { PaginatePipe } from '../pipes/paginate.pipe';
   declarations: [
     RestaurantListComponent,
     RestaurantDetailComponent,
-    RestaurantFormComponent, 
+    RestaurantFormComponent,
     PaginatePipe
   ],
   imports: [
@@ -23,8 +24,10 @@ import { PaginatePipe } from '../pipes/paginate.pipe';
     RestaurantsRoutingModule,
     MatCardModule,
     MatGridListModule,
-    MatPaginatorModule
-   
+    MatPaginatorModule,
+    MatListModule
+
+
   ],
   exports: [
     RestaurantListComponent,
