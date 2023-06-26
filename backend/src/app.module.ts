58ffbcbf5761668dfad7a78fc2fc64219cc4231f
Vanesa <vvalverde76@gmail.com>
2023-06-26 18:02:00 +0200

    creacion de modulos, restaurante, usuario, comentario y booking del backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { RestaurantsModule } from './restaurants/restaurants.module';
+import { UsersModule } from './users/users.module';
+import { CommentsModule } from './comments/comments.module';
+import { BookingsModule } from './bookings/bookings.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, RestaurantsModule, UsersModule, CommentsModule, BookingsModule],
   controllers: [AppController],
   providers: [AppService],
 })
