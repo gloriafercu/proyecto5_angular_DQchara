5eb2ea03417422a14bd49c84d58abb7b5a769f3f
vcrl1 <vcrl1994@gmail.com>
2023-06-30 13:50:05 +0200

    Actualización login

--- frontend/src/app/users/user-login/user-login.component.ts
+++ frontend/src/app/users/user-login/user-login.component.ts
@@ -1,21 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
   styleUrls: ['./user-login.component.css']
 })
-export class UserLoginComponent {
+export class UserLoginComponent{
+constructor (private router:Router){}
   hide = true
+
   userLoginForm = new FormGroup({
-    email: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]), 
     password: new FormControl('', [Validators.required, Validators.pattern('^[A-Za-z0-9$%&/()]{8,20}$')]),
   })
 
   save(): void {
-    let id = this.userLoginForm.get('email')?.value ?? 0;
-    let title = this.userLoginForm.get('password')?.value ?? '';
+    let email = this.userLoginForm.get('email')?.value ?? 0;
+    let password = this.userLoginForm.get('password')?.value ?? '';
+    console.log(email, password);
+
+//enrutar a la pantalla de inicio. 
+    console.log("te has conectado ok");
+    this.userLoginForm.reset()
+    this.router.navigate(['/restaurants'])
+
   }
 
 }
