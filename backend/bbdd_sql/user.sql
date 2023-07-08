039f347de843e33e953a4fc157ad1fab7877bdef
gloriafercu <gloriafercu@gmail.com>
2023-07-08 20:44:26 +0200

    user.sql generado

---
+++ backend/bbdd_sql/user.sql
@@ -0,0 +1,11 @@
+insert into `backend_dqchara`.`user` (`firstName`, `lastName`, `email`, `password`,`phone`, `userName`, `avatar`, `role`) values ('Maryanne', 'Ney', 'mney0@typepad.com', 'nK7)\&', '4912902552', 'mney0', 'https://robohash.org/quianequeest.png?size=50x50&set=set1', 'user');
+
+insert into `backend_dqchara`.`user` (`firstName`, `lastName`, `email`, `password`,`phone`, `userName`, `avatar`, `role`) values ('Sherri', 'Filipychev', 'sfilipychev1@icq.com', 'bM7)`?CwOC', '6388281927', 'sfilipychev1', 'https://robohash.org/magniearumodit.png?size=50x50&set=set1', 'admin');
+
+insert into `backend_dqchara`.`user` (`firstName`, `lastName`, `email`, `password`,`phone`, `userName`, `avatar`, `role`) values ('Winona', 'Wallentin', 'wwallentin2@list-manage.com', 'jJ5,TrX', '1622029828', 'wwallentin2', 'https://robohash.org/similiqueassumendavoluptate.png?size=50x50&set=set1', 'user');
+
+insert into `backend_dqchara`.`user` (`firstName`, `lastName`, `email`, `password`,`phone`, `userName`, `avatar`, `role`) values ('Cal', 'Milson', 'cmilson3@ca.gov', 'gR2>mgGwZD!p', '1535382682', 'cmilson3', 'https://robohash.org/etaliquamest.png?size=50x50&set=set1', 'user');
+
+insert into `backend_dqchara`.`user` (`firstName`, `lastName`, `email`, `password`,`phone`, `userName`, `avatar`, `role`) values ('Briano', 'Brennenstuhl', 'bbrennenstuhl4@google.de', 'lO8#VH', '9171816733', 'bbrennenstuhl4', 'https://robohash.org/facilisarchitectoquo.png?size=50x50&set=set1', 'admin');
+
+insert into `backend_dqchara`.`user` (`firstName`, `lastName`, `email`, `password`,`phone`, `userName`, `avatar`, `role`) values ('Graehme', 'Dalgliesh', 'gdalgliesh5@cafepress.com', 'dC5*)t1A.6Y', '1499902724', 'gdalgliesh5', 'https://robohash.org/etsintiure.png?size=50x50&set=set1', 'admin');
