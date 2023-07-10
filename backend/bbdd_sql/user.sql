78b7b6bf3dbc8cbff9337f11b05e470bb40dd7aa
Vanesa <vvalverde76@gmail.com>
2023-07-10 18:56:49 +0200

    contraseñas cifradas

--- backend/bbdd_sql/user.sql
+++ backend/bbdd_sql/user.sql
@@ -1,9 +1,9 @@
-insert into `backend_dqchara`.`user` (`firstName`, `lastName`, `email`, `password`,`phone`, `userName`, `avatar`, `role`) values ('Vanesa','Valverde','vanesav@hola.com','1234asdf','651232444','vanesav','https://robohash.org/quianequeest.png?size=50x50&set=set1','user');
+insert into `backend_dqchara`.`user` (`firstName`, `lastName`, `email`, `password`,`phone`, `userName`, `avatar`, `role`) values ('Vanesa','Valverde','vanesav@hola.com','$2b$10$LdkWumlpo/1COBaXrdCMbe4raJdr4ZIPfiwgxhZx/wwF34f1Ta8DC','651232444','vanesav','https://robohash.org/quianequeest.png?size=50x50&set=set1','user');
 
-insert into `backend_dqchara`.`user` (`firstName`, `lastName`, `email`, `password`,`phone`, `userName`, `avatar`, `role`) values ('Vanessa','Rodríguez','vcrl1@hola.com','1234asdf','611111111','vcrl1','https://robohash.org/magniearumodit.png?size=50x50&set=set1','admin');
+insert into `backend_dqchara`.`user` (`firstName`, `lastName`, `email`, `password`,`phone`, `userName`, `avatar`, `role`) values ('Vanessa','Rodríguez','vcrl1@hola.com','$2b$10$LdkWumlpo/1COBaXrdCMbe4raJdr4ZIPfiwgxhZx/wwF34f1Ta8DC','611111111','vcrl1','https://robohash.org/magniearumodit.png?size=50x50&set=set1','admin');
 
-insert into `backend_dqchara`.`user` (`firstName`, `lastName`, `email`, `password`,`phone`, `userName`, `avatar`, `role`) values ('Jeane','Bonfim','jeane@hola.com','1234asdf','655555550','jeane@hola.com','https://robohash.org/similiqueassumendavoluptate.png?size=50x50&set=set1','user');
+insert into `backend_dqchara`.`user` (`firstName`, `lastName`, `email`, `password`,`phone`, `userName`, `avatar`, `role`) values ('Jeane','Bonfim','jeane@hola.com','$2b$10$LdkWumlpo/1COBaXrdCMbe4raJdr4ZIPfiwgxhZx/wwF34f1Ta8DC','655555550','jeane@hola.com','https://robohash.org/similiqueassumendavoluptate.png?size=50x50&set=set1','user');
 
-insert into `backend_dqchara`.`user` (`firstName`, `lastName`, `email`, `password`,`phone`, `userName`, `avatar`, `role`) values ('Gloria','Fernández','gloriafc@hola.com','1234asdf','662288981','gloriafc','https://robohash.org/similiqueassumendavoluptate.png?size=50x50&set=set1','user');
+insert into `backend_dqchara`.`user` (`firstName`, `lastName`, `email`, `password`,`phone`, `userName`, `avatar`, `role`) values ('Gloria','Fernández','gloriafc@hola.com','$2b$10$LdkWumlpo/1COBaXrdCMbe4raJdr4ZIPfiwgxhZx/wwF34f1Ta8DC','662288981','gloriafc','https://robohash.org/similiqueassumendavoluptate.png?size=50x50&set=set1','user');
 
 
