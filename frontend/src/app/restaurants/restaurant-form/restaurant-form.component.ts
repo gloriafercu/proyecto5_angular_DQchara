b67f4e80d0ac829b871fed35fc423d68657bf015
vvalverde76 <vvalverde76@gmail.com>
2023-07-27 13:33:16 +0200

    comprobacion y ajuste de rutas para enlazar rest-form con rest-form-image

--- frontend/src/app/restaurants/restaurant-form/restaurant-form.component.ts
+++ frontend/src/app/restaurants/restaurant-form/restaurant-form.component.ts
@@ -146,12 +146,12 @@ export class RestaurantFormComponent implements OnInit {
     if (id === 0) // nuevo
       this.restaurantService.create(restaurant).subscribe(restaurant => {
         console.log(restaurant);
-        //this.router.navigate(['/restaurants/my-restaurant', restaurant.id, 'photos'])
-        this.router.navigate(['/restaurants', restaurant.id]);
+        this.router.navigate(['/restaurants/my-restaurant', restaurant.id, 'photos'])
+        //this.router.navigate(['/restaurants', restaurant.id]);
       });
     else // editar restaurante existente
       this.restaurantService.update(restaurant).subscribe(restaurant => {
-        this.router.navigate(['/restaurants/my-restaurant', restaurant.id, 'edit']);
+        //this.router.navigate(['/restaurants/my-restaurant', restaurant.id, 'edit']);
         this.router.navigate(['/restaurants', restaurant.id]);
       });
   }
