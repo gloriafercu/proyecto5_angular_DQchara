fff6f66f7b97fbc9186015282090f611c5c32d12
vvalverde76 <vvalverde76@gmail.com>
2023-10-02 10:05:09 +0200

    ajustes presentacion

--- backend/bbdd_sql/user.sql
+++ backend/bbdd_sql/user.sql
@@ -1,8 +1,15 @@
-insert into `backend_dqchara`.`user` (`firstName`, `lastName`, `email`, `password`,`phone`, `userName`, `avatar`, `role`) values ('Vanesa','Valverde','vanesav@hola.com','$2a$10$lfkeTt1dCfE6yYkTBfpk1.K28CKxlNqQYluReHQJPF1jVhz0zyKwa','651232444','vanesav','baca2b18-1eef-4a69-b6a5-6bacd2b27d39.png','user');
+insert into `backend_dqchara`.`user` (`firstName`, `lastName`, `email`, `password`,`phone`, `userName`, `avatar`, `role`,`id_restaurant`) values ('Vanesa','Valverde','vanesav@hola.com','$2a$10$lfkeTt1dCfE6yYkTBfpk1.K28CKxlNqQYluReHQJPF1jVhz0zyKwa','651232444','vanesav','baca2b18-1eef-4a69-b6a5-6bacd2b27d39.png','user');
 
-insert into `backend_dqchara`.`user` (`firstName`, `lastName`, `email`, `password`,`phone`, `userName`, `avatar`, `role`) values ('Vanessa','Rodríguez','vcrl1@hola.com','$2a$10$lfkeTt1dCfE6yYkTBfpk1.K28CKxlNqQYluReHQJPF1jVhz0zyKwa','611111111','vcrl1','dc41cb84-5921-4286-9c07-a409526ea4e2.png','admin');
+insert into `backend_dqchara`.`user` (`firstName`, `lastName`, `email`, `password`,`phone`, `userName`, `avatar`, `role`,`id_restaurant`) values ('Vanessa','Rodríguez','vcrl1@hola.com','$2a$10$lfkeTt1dCfE6yYkTBfpk1.K28CKxlNqQYluReHQJPF1jVhz0zyKwa','611111111','vcrl1','dc41cb84-5921-4286-9c07-a409526ea4e2.png','admin');
 
-insert into `backend_dqchara`.`user` (`firstName`, `lastName`, `email`, `password`,`phone`, `userName`, `avatar`, `role`) values ('Jeane','Bonfim','jeane@hola.com','$2a$10$lfkeTt1dCfE6yYkTBfpk1.K28CKxlNqQYluReHQJPF1jVhz0zyKwa','655555550','jeane','da331137-0e33-4664-a415-ef780c09a76e.png','user');
+insert into `backend_dqchara`.`user` (`firstName`, `lastName`, `email`, `password`,`phone`, `userName`, `avatar`, `role`,`id_restaurant`) values ('Jeane','Bonfim','jeane@hola.com','$2a$10$lfkeTt1dCfE6yYkTBfpk1.K28CKxlNqQYluReHQJPF1jVhz0zyKwa','655555550','jeane','da331137-0e33-4664-a415-ef780c09a76e.png','user');
 
-insert into `backend_dqchara`.`user` (`firstName`, `lastName`, `email`, `password`,`phone`, `userName`, `avatar`, `role`) values ('Gloria','Fernández','gloriafc@hola.com','$2a$10$lfkeTt1dCfE6yYkTBfpk1.K28CKxlNqQYluReHQJPF1jVhz0zyKwa','662288981','gloriafc','0dc51584-8b6a-4010-a59f-fe9f2c1d4bb4.png','user');
+insert into `backend_dqchara`.`user` (`firstName`, `lastName`, `email`, `password`,`phone`, `userName`, `avatar`, `role`,`id_restaurant`) values ('Gloria','Fernández','gloriafc@hola.com','$2a$10$lfkeTt1dCfE6yYkTBfpk1.K28CKxlNqQYluReHQJPF1jVhz0zyKwa','662288981','gloriafc','0dc51584-8b6a-4010-a59f-fe9f2c1d4bb4.png','user');
 
+insert into `backend_dqchara`.`user` (`firstName`, `lastName`, `email`, `password`,`phone`, `userName`, `avatar`, `role`,`id_restaurant`) values ('Julian','Lopez','info@rocacho.com','$2a$10$lfkeTt1dCfE6yYkTBfpk1.K28CKxlNqQYluReHQJPF1jVhz0zyKwa','666000111','rocacho','0dc51584-8b6a-4010-a59f-fe9f2c1d4bb4.png','rest',`31`);
+
+insert into `backend_dqchara`.`user` (`firstName`, `lastName`, `email`, `password`,`phone`, `userName`, `avatar`, `role`,`id_restaurant`) values ('Alfonso','Garcia','info@elpimientoverde.com','$2a$10$lfkeTt1dCfE6yYkTBfpk1.K28CKxlNqQYluReHQJPF1jVhz0zyKwa','666000222','pimientoverde','0dc51584-8b6a-4010-a59f-fe9f2c1d4bb4.png','rest',`32`);
+
+insert into `backend_dqchara`.`user` (`firstName`, `lastName`, `email`, `password`,`phone`, `userName`, `avatar`, `role`,`id_restaurant`) values ('Donatella','Rossi','info@canpizza.com','$2a$10$lfkeTt1dCfE6yYkTBfpk1.K28CKxlNqQYluReHQJPF1jVhz0zyKwa','666000333','canpizza','0dc51584-8b6a-4010-a59f-fe9f2c1d4bb4.png','rest',`33`);
+
+insert into `backend_dqchara`.`user` (`firstName`, `lastName`, `email`, `password`,`phone`, `userName`, `avatar`, `role`,`id_restaurant`) values ('Joan','Roca','info@canroca.com','$2a$10$lfkeTt1dCfE6yYkTBfpk1.K28CKxlNqQYluReHQJPF1jVhz0zyKwa','666000444','canroca','0dc51584-8b6a-4010-a59f-fe9f2c1d4bb4.png','rest',`34`);
